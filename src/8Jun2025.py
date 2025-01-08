c0f2db822aa9c09244ca6ff7a3845485278d3d04
zahra <zh.dehghani741@gmail.com>
2025-01-08 23:46:23 +0330

    add 8Jun

---
+++ src/8Jun2025.py
@@ -0,0 +1,18 @@
+from unicodedata import digit
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1: list[int], l2: list[int]) -> list[int]:
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        digit1 = int("".join(map(str, reversed(l1))))
+        digit2 = int("".join(map(str, reversed(l2))))
+
+        final_digit = digit1 + digit2
+        return list(map(int, str(final_digit)[::-1]))
+
+
+print(Solution().addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]))
